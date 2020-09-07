fffc258a7bf12e70b1e8ddcb4e4e318a8fa8ae60
Ammach Mohamed Amine <maammach@norsys.fr>
2020-09-07 09:30:17 +0100

    FormWizard: create HeaderWithCategory component

--- src/pages/form/FormWizard.js
+++ src/pages/form/FormWizard.js
@@ -1,29 +1,17 @@
 import React, { Fragment, useEffect, useState } from "react";
-import { Link, useHistory } from "react-router-dom";
-import { Button as ButtonAntd } from "antd";
-import { InfoOutlined, HomeOutlined } from "@ant-design/icons";
+import { useHistory } from "react-router-dom";
 import { useWindowSize } from "@hooks/window";
 import { getCategoryItems } from "@utils/category";
-import { Header } from "@components/header/Header";
 import { BoxSides } from "@components/box/BoxSides";
 import { DynamicSummary } from "@components/dynamicSummary/DynamicSummary";
 import { MobileDynamicSummary } from "@components/dynamicSummary/MobileDynamicSummary";
+import { HeaderWithCategory } from "@components/header/HeaderWithCategory";
 import { FooterWithNavigation } from "@components/footer/FooterWithNavigation";
-import { Modal } from "@components/modal/Modal";
-import { StyledTitle } from "@components/title/StyledTitle";
 import { config } from "./formConfig";
-
-import {
-  ABOUT_TITLE,
-  ABOUT_CONTENT1,
-  ABOUT_CONTENT2,
-  ABOUT_CONTENT3,
-} from "@utils/constants";
 
 export function FormWizard() {
   const isMobile = useWindowSize();
   const history = useHistory();
-  const [isVisible, setVisible] = useState(false);
   const [activeStep, setActiveStep] = useState(0);
 
   useEffect(() => {
@@ -84,36 +72,7 @@ export function FormWizard() {
   const [title1, title2] = category.split(" ");
   return (
     <Fragment>
-      <Header>
-        <div
-          style={{
-            position: "fixed",
-            top: "8px",
-            left: "8px",
-            color: "var(--main-color)",
-            fontFamily: "Paris2024",
-          }}
-        >
-          <Link to="/intro">
-            <ButtonAntd type="primary" shape="circle" icon={<HomeOutlined />} />
-            <span style={{ marginLeft: "4px" }}>PARIS2024</span>
-          </Link>
-        </div>
-        <ButtonAntd
-          className="show-modal-btn"
-          type="primary"
-          shape="circle"
-          icon={<InfoOutlined />}
-          onClick={() => setVisible(true)}
-        />
-        <StyledTitle title1={title1} title2={title2} />
-      </Header>
-      <Modal
-        title={ABOUT_TITLE}
-        content={[ABOUT_CONTENT1, ABOUT_CONTENT2, ABOUT_CONTENT3]}
-        isVisible={isVisible}
-        hideOverlay={() => setVisible(false)}
-      />
+      <HeaderWithCategory title1={title1} title2={title2} />
       {content}
       <FooterWithNavigation
         previous={{
